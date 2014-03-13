0458174c5244fc99ed9a27ed816485d8e4c0ae1c
WindBeat <jordanwalker465@gmail.com>
2014-03-12 21:19:35 -0700

    html/css/js for custom player

---
+++ soundcloud.js
@@ -0,0 +1,18 @@
+function playSomeMusic(myGenre) {  
+
+  var url = 'https://api.soundcloud.com/tracks.json?client_id=02f6c269172ac9312f9de8ac41324001';
+	$.getJSON(url, { genres: myGenre, bpm: { from: 120 } },
+  function(tracks) {
+    //Pulls 50 songs and then randomly chooses one
+      var random = Math.floor(Math.random() * 49);
+      var track_url = tracks[random].permalink_url;
+      //Displays the sound player
+      SC.stream(track_url, function(sound) {
+        $('#play').click(function(e) {
+          e.preventDefault();
+          sound.start();
+
+      });    
+    });        
+  });
+};
